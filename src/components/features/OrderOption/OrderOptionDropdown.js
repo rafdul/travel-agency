3c38aa41edaf579608643b2d821519bef2419b25
rafdul <rafalduluk@gmail.com>
2021-01-08 13:39:38 +0100

    Add rendered element in OrderOptionIcons

--- src/components/features/OrderOption/OrderOptionDropdown.js
+++ src/components/features/OrderOption/OrderOptionDropdown.js
@@ -15,7 +15,10 @@ const OrderOptionDropdown = (currentValue, setOptionValue, required, values) => 
       <option key='null' value=''>---</option>
     )}
     {values.map(value => (
-      <option key={value.id} value={value.id}>{value.name} ({formatPrice(value.price)})</option>
+      <option key={value.id} value={value.id}>
+        {value.name}
+        ({formatPrice(value.price)})
+      </option>
     ))}
   </select>
 );
--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import Icon from '../../Icon/Icon';
+import {formatPrice} from '../../../utils/formatPrice';
 
+const OrderOptionIcons = ({required, currentValue, values, setOptionValue}) => (
 
-const OrderOptionIcons = () => (
-
-  <div>OrderOptionIcons</div>
+  <div className={styles.component}>
+    {required ? '' : (
+      <div
+        className={styles.icon}
+        onClick={() => setOptionValue('')}
+      >
+        <Icon name="times-circle">
+          none
+        </Icon>
+      </div>
+    )}
+    {values.map(value => (
+      <div
+        className={`${styles.icon} ${currentValue == value.id ? styles.iconActive : ''}`}
+        key={value.id}
+        onClick={event => setOptionValue(event.currentTarget.value.id)}
+      >
+        <Icon name={value.icon}>
+          {value.name}
+          ({formatPrice(value.price)})
+        </Icon>
+      </div>
+    ))}
+  </div>
 );
 
 OrderOptionIcons.propTypes = {
-  name: PropTypes.string,
+  required: PropTypes.bool,
+  currentValue: PropTypes.string,
+  values: PropTypes.array,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionIcons;
