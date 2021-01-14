52f4d4669338708dd42083f53afbbd4488999e24
rafdul <rafalduluk@gmail.com>
2021-01-14 14:08:38 +0100

    Make test component TripSummary

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -1,3 +1,40 @@
-// import React from 'react';
-// import {shallow} from 'enzyme';
-// import Hero from './Hero';
+import React from 'react';
+import {shallow} from 'enzyme';
+import TripSummmary from './TripSummary';
+// import {Link} from 'react-router-dom';
+
+describe('Component TripSummary', () => {
+  it('should render correct url', () => {
+    const componentId = 'xyz';
+    const expectedUrl = '/trip/xyz';
+    const component = shallow(<TripSummmary id={componentId} tags={['tag1','tag2']}/>);
+    expect(component.find('.link').prop('to')).toEqual(expectedUrl);
+    // expect(component.find('Link').prop('to')).toEqual(expectedUrl);
+
+    // console.log(component.debug());
+  });
+
+  it('should render img with correct src and alt', () => {
+    const component = shallow(<TripSummmary image={'Lorem'} name={'Ipsum'} tags={['tag1','tag2']}/>);
+    expect(component.find('img').prop('src')).toEqual('Lorem');
+    expect(component.find('img').prop('alt')).toEqual('Ipsum');
+    // console.log(component.debug());
+  });
+
+  it('should render correct name, cost, days', () => {
+    const compName = 'USA';
+    const compCost = '$10.000';
+    const compDays = 7;
+    const component = shallow(<TripSummmary name={compName} cost={compCost} days={compDays} tags={['tag1','tag2']}/>);
+    expect(component.find('h3.title').text()).toEqual(compName);
+    expect(component.find('.details span:first-child').text()).toEqual(compDays + ' days');
+    expect(component.find('.details span:last-child').text()).toEqual('from ' + compCost);
+    // expect(component.find('.details span').at(0).text()).toEqual(compDays + ' days');
+    // expect(component.find('.details span').at(1).text()).toEqual('from ' + compCost);
+    // console.log(component.debug());
+  });
+
+  it('should throw error without props', () => {
+    expect(() => shallow(<TripSummmary />)).toThrow();
+  });
+});
