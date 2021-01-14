7fc1942150297fd0707ec81fde473b52d9306e81
rafdul <rafalduluk@gmail.com>
2021-01-14 23:05:25 +0100

    Add test subcomponents in OrderOption

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import OrderOption from './OrderOption';
 
+/* testy dla komponentu głównego (OrderOption); testy subkomponentów poniżej */
 describe('Component OrderOption', () => {
   it('should render without crashing', () => {
     const component = shallow(<OrderOption type='dropdown' name='nazwa_komponentu' />);
     expect(component).toBeTruthy();
-    console.log(component.debug());
+    // console.log(component.debug());
   });
 
   it('should return empty object if called without required props', () => {
@@ -17,8 +18,104 @@ describe('Component OrderOption', () => {
   it('should render correct title', () => {
     const compTitle = 'Lorem ipsum';
     const component = shallow(<OrderOption type='dropdown' name={compTitle} />);
-
     expect(component.find('h3.title').text()).toEqual(compTitle);
-
   });
 });
+
+/*testy dla subkomponentów*/
+// pętla, która pomoże nam testować każdy z subkomponentów znajdujących się w OrderOption
+const optionTypes = {
+  dropdown: 'OrderOptionDropdown',
+  icons: 'OrderOptionIcons',
+  checkboxes: 'OrderOptionCheckboxes',
+  number: 'OrderOptionNumber',
+  text: 'OrderOptionText',
+  date: 'OrderOptionDate',
+};
+
+// obiekt zawierający wiele propsów, które chcemy nadawać subkomponentom
+// wykorzystywane do mockowania: mockProps, mockPropsForType, testValue, testValueNumber
+const mockProps = {
+  id: 'abc',
+  name: 'Lorem',
+  values: [
+    {id: 'aaa', icon: 'h-square', name: 'Lorem A', price: 0},
+    {id: 'xyz', icon: 'h-square', name: 'Lorem X', price: 100},
+  ],
+  required: false,
+  currentValue: 'aaa',
+  price: '50%',
+  limits: {
+    min: 0,
+    max: 6,
+  },
+};
+
+const mockPropsForType = {
+  dropdown: {},
+  icons: {},
+  checkboxes: {currentValue: [mockProps.currentValue]},
+  number: {currentValue: 1},
+  text: {},
+  date: {},
+};
+
+const testValue = mockProps.values[1].id;
+// const testValueNumber = 3;
+
+for(let type in optionTypes){
+  describe(`Component OrderOption with type=${type}`, () => {
+    /* test setup */
+    let component;
+    let subcomponent;
+    let renderedSubcomponent;
+    let mockSetOrderOption;
+
+    beforeEach(() => {
+      mockSetOrderOption = jest.fn();
+      component = shallow(
+        <OrderOption
+          type={type}
+          setOrderOption={mockSetOrderOption}
+          {...mockProps}
+          {...mockPropsForType[type]}
+        />
+      );
+      subcomponent = component.find(optionTypes[type]);
+      renderedSubcomponent = subcomponent.dive();
+    });
+
+    /* common tests */
+    it(`renders ${optionTypes[type]}`, () => {
+      expect(subcomponent).toBeTruthy();
+      expect(subcomponent.length).toBe(1);
+      // console.log(component.debug());
+      // console.log(subcomponent.debug());
+    });
+
+    /* type-specific tests */
+    switch (type) {
+      case 'dropdown': {
+        it('contains select and options', () => {
+          const select = renderedSubcomponent.find('select');
+          expect(select.length).toBe(1);
+
+          const emptyOption = select.find('option[value=""]').length;
+          expect(emptyOption).toBe(1);
+
+          const options = select.find('option').not('[value=""]');
+          expect(options.length).toBe(mockProps.values.length);
+          expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
+          expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+        });
+
+        it('should run setOrderOption function on change', () => {
+          renderedSubcomponent.find('select').simulate('change', {currentTarget: {value: testValue}});
+          expect(mockSetOrderOption).toBeCalledTimes(1);
+          expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+        });
+        break;
+      }
+    }
+  });
+}
