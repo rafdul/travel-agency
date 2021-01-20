0404679c4c640c395dee13781c8a0a3214a7d232
rafdul <rafalduluk@gmail.com>
2021-01-20 14:38:06 +0100

    Add test heading and countdown in HappyHourAd

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,14 +1,23 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './HappyHourAd.scss';
 
 class HappyHourAd extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+  };
+
+  static defaulProps = {
+    title: 'Happy Hour!',
+  }
 
   render(){
+    const { title } = this.props;
+
     return (
       <div className={styles.component}>
-        <p>Happy Hour</p>
-        <p>Time</p>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.promoDescription}></div>
       </div>
     );
   }
--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,10 +2,36 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  countdown: '.countdown',
+  promoDescription: '.promoDescription',
+};
+
+const mockProps = {
+  title: 'Happy Hour Lorem ipsum',
+  promoDescription: 'It`s your time! Take advantage of Happy Hour! All offers 20% off!',
+};
+
 describe('Component HappyHourAd', () => {
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
     // console.log(component.debug());
+  });
+
+  it('should render title and countdown', () => {
+    const component = shallow(<HappyHourAd {...mockProps}/>);
+    expect(component.exists(select.title)).toBe(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
+    // expect(component.find('h3').hasClass('title')).toEqual(true);
+    // expect(component.find('div').some('.countdown')).toEqual(true);
+    // console.log(component.debug());
+  });
+
+  it('should render correct heading from props', () => {
+    const component = shallow(<HappyHourAd {...mockProps} />);
+    expect(component.find(select.title).text()).toEqual(mockProps.title);
+    console.log(component.debug());
   });
 });
